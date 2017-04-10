d04d1d067d64d12cbe4b076af23b3dbe698348a2
Jason Corey <jason@Jasons-MacBook-Air.local>
2017-04-10 10:27:42 -0400

    style and jquery stuff

--- public/script/script.js
+++ public/script/script.js
@@ -1,17 +1,21 @@
 console.log('working')
 
 $('.link1').click(function(){
-	console.log('clicked')
+	window.location.href = '/library/utilities'
+	return false
 })
 
 $('.link2').click(function(){
-	console.log('clicked')
+	window.location.href = '/library/media'
+	return false
 })
 
 $('.link3').click(function(){
-	console.log('clicked')
+	window.location.href = '/library/design'
+	return false
 })
 
 $('.link4').click(function(){
-	console.log('clicked')
+	window.location.href = '/library/fun'
+	return false
 })
